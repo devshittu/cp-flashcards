717980c312a20698177bffd756db4fb786c8bcde
Muhammed Shittu <devshittu@gmail.com>
2023-12-09 01:46:19 +0000

    added feature quize slice

---
+++ src/features/quizzes/quizzesSlice.js
@@ -0,0 +1,24 @@
+// src/features/quizzes/quizzesSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const quizzesSlice = createSlice({
+  name: "quizzes",
+  initialState: {
+    quizzes: {},
+  },
+  reducers: {
+    addQuiz: (state, action) => {
+      const { id, name, topicId, cardIds } = action.payload;
+      state.quizzes[id] = {
+        id,
+        name,
+        topicId,
+        cardIds,
+      };
+    },
+  },
+});
+
+export const { addQuiz } = quizzesSlice.actions;
+export const selectQuizzes = (state) => state.quizzes.quizzes;
+export default quizzesSlice.reducer;
